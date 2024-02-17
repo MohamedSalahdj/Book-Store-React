45e4475174c942bfd0865c1d9039354caaffd5d1
alibayomy <alibayomy12@gmail.com>
2024-02-18 01:23:28 +0200

    Desgin the home page of our project, With the nav bar

--- bookstore/src/App.js
+++ bookstore/src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import HomePage from './Pages/HomePage/HomePage';
+import NavBarComponent from './Components/NavBarComponent';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <BrowserRouter>
+      <NavBarComponent></NavBarComponent>
+      <HomePage></HomePage>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ bookstore/src/Network/AxiosInstance.js
@@ -0,0 +1,19 @@
+
+
+import axios from 'axios';
+
+// export const AxiosInstance = {
+//   method: 'GET',
+//   url: 'https://books-api7.p.rapidapi.com/books/get/random/',
+//   headers: {
+//     'X-RapidAPI-Key': '0b30170f63msh062078794f6271bp163733jsn8510c750f501',
+//     'X-RapidAPI-Host': 'books-api7.p.rapidapi.com'
+//   }
+// };
+
+// try {
+// 	const response = await axios.request(AxiosInstance);
+// 	console.log(response.data);
+// } catch (error) {
+// 	console.error(error);
+// }
