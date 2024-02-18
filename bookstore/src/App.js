81c33d18ddec85c04920688f67cf95804073a050
shahdahmedabdelraoufmohamed <shahd3a7med@gmail.com>
2024-02-18 09:11:03 +0200

    Enhance style of 'Create a new account?' section

--- bookstore/src/App.js
+++ bookstore/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
-import ClientSelectComponent from './Pages/ClientSelect/ClientSelectComponent';
+import Login from './Pages/Login/Login';
+
 import { BrowserRouter, Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
-import RegitserComponent from './Pages/Register/RegisterComponent';
+
 
 function App() {
   return (
+    <div className="App">
+   
     <>
       <BrowserRouter>
-        <Switch>
-          <Route exact path={'/'} component={ClientSelectComponent} />
-          <Route exact path={'/Register/:userType'} component={RegitserComponent} />
-        </Switch>
+      <Login/>
+
       
       </BrowserRouter>
     
     </>
-    
-  )
+    </div>
+  );
 }
 
 export default App;
--- bookstore/src/Components/MessageErrorcomponent.js
+++
@@ -1,7 +0,0 @@
-
-function MessageErrorComponent(props){
-    return (
-        <p className={`text-${props.classErrorMessage} p-1`}>{props.messageError}</p>
-    )
-}
-export default MessageErrorComponent;
