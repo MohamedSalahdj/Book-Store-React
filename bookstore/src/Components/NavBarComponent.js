82ebe35c8ed6a99fb7c63b838aa6b14e1f0f1727
alibayomy <alibayomy12@gmail.com>
2024-02-19 01:12:16 +0200

    sharing the same buttons structre and adding links to the nav bar

--- bookstore/src/Components/NavBarComponent.js
+++ bookstore/src/Components/NavBarComponent.js
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 import './NavBarComponent.css'
 
 
@@ -5,27 +7,29 @@ function NavBarComponent() {
     return (<>
         <nav class="navbar navbar-expand-lg sticky-top  mynavBar  navbar-dark bg-transparent justify-content-center">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">Navbar</a>
                 <button class="navbar-toggler btn-white text-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0 text-white">
                         <li class="nav-item">
-                            <a class="nav-link active text-white" aria-current="page" href="#">Home</a>
+                            <Link class="nav-link active text-white" aria-current="page" to="/">Home</Link>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Link</a>
+                            <Link class="nav-link" to="/register">Register</Link>
+                        </li>
+                        <li class="nav-item">
+                            <Link class="nav-link" to="/login">login</Link>
                         </li>
                         <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <Link class="nav-link dropdown-toggle" to="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                 Categories
-                            </a>
+                            </Link>
                             <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#">Action</a></li>
-                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                                <li><Link class="dropdown-item" to="#">Action</Link></li>
+                                <li><Link class="dropdown-item" to="#">Another action</Link></li>
                                 <li><hr class="dropdown-divider"></hr></li>
-                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                <li><Link class="dropdown-item" to="#">Something else here</Link></li>
                             </ul>
                         </li>
                     </ul>
