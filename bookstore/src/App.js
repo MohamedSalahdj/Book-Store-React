89d01b07890680f99eaaa247b92e234b86ec64b6
nasser30 <mohamedsalheen1@gmail.com>
2024-02-18 23:09:10 +0200

    add view book details

--- bookstore/src/App.js
+++ bookstore/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import "./App.css";
+import ViewBook from "./Pages/ViewBook/ViewBook";
+import {
+  BrowserRouter,
+  Route,
+  Switch,
+} from "react-router-dom/cjs/react-router-dom.min";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path={"/details/:id"} component={ViewBook}></Route>
+        </Switch>
+      </BrowserRouter>
+
+      {/* <ViewBook /> */}
     </div>
   );
 }
--- bookstore/src/Pages/ViewBook/ViewBook.js
+++ bookstore/src/Pages/ViewBook/ViewBook.js
@@ -1,10 +1,61 @@
-import React from "react";
+import React, { useEffect } from "react";
+import "./ViewBook.css";
+import { Link, useParams } from "react-router-dom/cjs/react-router-dom.min";
+import { useState } from "react";
+import axios from "axios";
 
 const ViewBook = () => {
+  const lol = () => {};
+
+  const bookId = useParams();
+  // console.log(bookId);
+
+  const [details, setDetails] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`https://retoolapi.dev/64GJU5/data/${bookId.id}`)
+      .then((res) => setDetails(res.data))
+      .catch((err) => console.log(err));
+  }, [bookId.id]);
+
   return (
-    <div>
-      <h1>Hello From View Book Page</h1>
-    </div>
+    <>
+      {/* <!-------------------- single product details ---------------------> */}
+      <div className="small-container single-product">
+        <div className="row">
+          <div className="col-2">
+            <img
+              src={details.images}
+              alt="product details"
+              width="100%"
+              id="ProductImg"
+            />
+          </div>
+          <div className="col-2">
+            <p>Home / Books</p>
+            {/* <h1>Red Printd T-Shirt by HRX</h1> */}
+            <h1>{details.title}</h1>
+            {/* <h4>$50.00</h4> */}
+            <h4>{details.price}$</h4>
+
+            <input type="number" value="1" onChange={lol} />
+            <Link to="" className="btn">
+              Add To Cart
+            </Link>
+            <h3>
+              Product Details <i className="fa fa-indent"></i>
+            </h3>
+            {/* <p>
+                  Give your summer wardrobe a style upgrade with the HRX Men's
+                  Active T-shirt. Team it with a pair of shorts for your morning
+                  workout or a denims for an evening out with the guys.
+                </p> */}
+            <p>{details.details}</p>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
