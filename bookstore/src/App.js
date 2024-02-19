d4cbab4b84ae3aa20692b71e6fcfdb4548033108
nasser30 <mohamedsalheen1@gmail.com>
2024-02-19 02:31:09 +0200

    Edit in view book css

--- bookstore/src/App.js
+++ bookstore/src/App.js
@@ -1,4 +1,4 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
